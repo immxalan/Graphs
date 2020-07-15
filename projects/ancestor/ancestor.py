68ba532bd8b7d6c5cb9405e6c42ada9b350385e0
Allen Do <57642471+immxalan@users.noreply.github.com>
2020-07-15 12:52:14 -0700

    Day 2 done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,33 @@
+from util import Stack, Queue
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        self.vertices[v1].add(v2)
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        graph.add_edge(pair[1], pair[0])
+    earliestAncestor = -1
+    maxLength = 1
+    q = Queue()
+    q.enqueue([starting_node])
+    while q.size() > 0:
+        path = q.dequeue()
+        vertex = path[-1]
+        if (len(path) >= maxLength and earliestAncestor != -1) or (len(path) > maxLength):
+            earliestAncestor = vertex
+            maxLength = len(path)
+        for neighbor in graph.vertices[vertex]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliestAncestor
